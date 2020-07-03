7d71da243aae7c19d42e1060605336cb11b19d24
nishit <nishitmohanan@gmail.com>
2020-07-04 00:07:47 +0530

    converted story to pure component for reference

--- components/story.js
+++ components/story.js
@@ -2,64 +2,72 @@ import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity, Linking} from 'react-native';
 import Icon from 'react-native-vector-icons/dist/FontAwesome5';
 
-const Story = props => {
-  return (
-    <TouchableOpacity style={styles.listItem}>
-      <View style={styles.listItemView}>
-        <Text
-          style={styles.listItemText}
-          //   onPress={() => Linking.openURL(props.item.hckr_url)}
-          onPress={() =>
-            props.navigation.navigate('Details', {story_detail: props.item})
-          }>
-          {props.story_rank + 1}) {props.item.title}
-        </Text>
-      </View>
-      <View style={styles.listItemScore}>
-        <Icon
-          name="heart"
-          size={20}
-          color="firebrick"
-          style={styles.listItemIcon}
-          //   onPress={() => props.deleteItem(props.item.id)}
-        />
-        <Text style={styles.listItemText}>{props.item.score}</Text>
+class Story extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+  styles = StyleSheet.create({
+    listItem: {
+      padding: 15,
+      backgroundColor: '#dfe4eb',
+      borderBottomWidth: 1,
+      borderColor: '#eee',
+    },
+    listItemView: {
+      justifyContent: 'space-between',
+      flex: 1,
+    },
+    listItemText: {
+      fontSize: 15,
+      fontWeight: 'bold',
+    },
+    listItemIcon: {
+      paddingHorizontal: 10,
+    },
+    listItemScore: {
+      flex: 1,
+      flexDirection: 'row',
+    },
+  });
+  render() {
+    return (
+      <TouchableOpacity style={this.styles.listItem}>
+        <View style={this.styles.listItemView}>
+          <Text
+            style={this.styles.listItemText}
+            //   onPress={() => Linking.openURL(this.props.item.hckr_url)}
+            onPress={() =>
+              this.props.navigation.navigate('Details', {
+                story_detail: this.props.item,
+              })
+            }>
+            {this.props.story_rank + 1})
+            {this.props.item.title}
+          </Text>
+        </View>
+        <View style={this.styles.listItemScore}>
+          <Icon
+            name="heart"
+            size={20}
+            color="firebrick"
+            style={this.styles.listItemIcon}
+            //   onPress={() => this.props.deleteItem(this.props.item.id)}
+          />
+          <Text style={this.styles.listItemText}>{this.props.item.score}</Text>
 
-        <Icon
-          name="comment-alt"
-          size={20}
-          color="firebrick"
-          style={styles.listItemIcon}
-          //   onPress={() => props.deleteItem(props.item.id)}
-        />
-        <Text style={styles.listItemText}>{props.item.score}</Text>
-      </View>
-    </TouchableOpacity>
-  );
-};
-
-const styles = StyleSheet.create({
-  listItem: {
-    padding: 15,
-    backgroundColor: '#dfe4eb',
-    borderBottomWidth: 1,
-    borderColor: '#eee',
-  },
-  listItemView: {
-    justifyContent: 'space-between',
-    flex: 1,
-  },
-  listItemText: {
-    fontSize: 15,
-    fontWeight: 'bold',
-  },
-  listItemIcon: {
-    paddingHorizontal: 10,
-  },
-  listItemScore: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-});
+          <Icon
+            name="comment-alt"
+            size={20}
+            color="firebrick"
+            style={this.styles.listItemIcon}
+            //   onPress={() => this.props.deleteItem(this.props.item.id)}
+          />
+          <Text style={this.styles.listItemText}>{this.props.item.score}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
 
 export default Story;
+// export const Story = React.memo(_Story);
